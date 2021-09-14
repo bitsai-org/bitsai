73d4949dfc8e25e48a1a7211df9111a540dec73d
Adoliin <ben.meft0@gmail.com>
2021-09-14 01:37:23 +0100

    add copy to clipboard notification component

---
+++ src/components/Receive/CopyCpNotification.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
+
+const Alert = (props: AlertProps): JSX.Element => {
+  return (
+    <MuiAlert elevation={6} variant="filled" {...props} />
+  )
+}
+
+interface Props {
+  open: boolean,
+  onClose: (open: boolean) => void,
+}
+
+const CopyCpNotification = (props: Props): JSX.Element => {
+
+  const handleClose = (
+    event: React.SyntheticEvent | React.MouseEvent,
+    reason?: string
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    props.onClose(false)
+  }
+
+  return (
+    <>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        open={props.open}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        message="Note archived"
+      >
+        <Alert onClose={handleClose} severity="info">
+          Copied address to clipboard
+        </Alert>
+      </Snackbar>
+      {/*<Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={props.open}
+        autoHideDuration={1000}
+        onClose={handleClose}
+        message="Note archived"
+        action={
+          <>
+            <Button color="secondary" size="small" onClick={handleClose}>
+              UNDO
+            </Button>
+            <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </>
+        }
+      />*/}
+    </>
+  )
+}
+
+export default CopyCpNotification
