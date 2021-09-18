e45a0993daffc4a423b69c3d680f52f79c8df5cf
Adoliin <ben.meft0@gmail.com>
2021-09-18 12:31:06 +0100

    add History component

--- src/views/History.tsx
+++ src/views/History.tsx
@@ -1,11 +1,25 @@
+import React from 'react'
+
+import HistoryTable from '../components/HistoryC/HistoryTable'
+
 import Box from '@material-ui/core/Box'
 
 const HistoryC = (): JSX.Element => {
+
   return (
-    <Box fontSize="5rem">
-      history
-    </Box>
+    <>
+      <Box fontSize="2rem" mb="2rem" display="flex" justifyContent="center">
+        <h2>
+          Transaction History
+        </h2>
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        {/*<Box display="flex" flexDirection="row">
+        </Box>*/}
+        <HistoryTable />
+      </Box>
+    </>
   )
 }
 
-export default HistoryC
+export default HistoryC;
