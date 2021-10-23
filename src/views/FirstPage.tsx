391474b258657d0220545f40c6fd6151982ba3b3
Adoliin <ben.meft0@gmail.com>
2021-10-23 16:18:18 +0100

    add some margins

--- src/views/FirstPage.tsx
+++ src/views/FirstPage.tsx
@@ -1,3 +1,7 @@
+import {useState, useEffect} from 'react'
+
+import Login from '../components/FirstPage/Login'
+
 //import theme from './../theme'
 import AddCircleIcon from '@material-ui/icons/AddCircle'
 import AttachFileIcon from '@material-ui/icons/AttachFile'
@@ -7,7 +11,19 @@ import Button from '@material-ui/core/Button'
 import styled from 'styled-components'
 import {Link} from 'react-router-dom'
 
+import persistedWallet from '../persist/data'
+
 const FirstPage = (): JSX.Element => {
+
+  const [persistedWalletExists, setPersistedWalletExists] = useState(false)
+  const [encryptedWallet, setEncryptedWallet] = useState<string | undefined>(undefined)
+
+  useEffect(() => {
+    const res = persistedWallet.getEncryptedWallet()
+    if (res)
+      setEncryptedWallet(res)
+  }, [])
+
   return (
     <Box
       display="flex"
@@ -17,14 +33,22 @@ const FirstPage = (): JSX.Element => {
       flexDirection="column"
     >
       <Box
-        marginTop="20vh"
+        mt="20vh"
+        mb="4rem"
         fontSize="5rem"
       >
         Bitcoin Wallet
       </Box>
 
+      {encryptedWallet &&
+      <Login
+        walletName="ado-btc"
+        encryptedWallet={encryptedWallet}
+      />
+      }
+
       <Box
-        marginTop="15vh"
+        mt="2rem"
         display="flex"
       >
         <StyledLink to="/generate-wallet">
