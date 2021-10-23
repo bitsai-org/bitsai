bf05894a275cb6b9a20bcc21ac7074d58127c886
Adoliin <ben.meft0@gmail.com>
2021-10-23 16:16:06 +0100

    add unAuthenticated routes with the help of auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,95 @@
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
+import { HashRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom"
+import {useEffect, useState} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+
 import styled from 'styled-components'
 
 import Home from './views/Home'
+//import
+import FirstPage from './views/FirstPage'
+import GenerateWallet from './views/GenerateWallet'
 import Send from './views/Send'
 import Receive from './views/Receive'
 import HistoryC from './views/HistoryC'
 
 import Box from '@material-ui/core/Box'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+import LockIcon from '@material-ui/icons/Lock'
 import IconButton from '@material-ui/core/IconButton'
 
-function App() {
+import auth from './persist/auth'
+
+const StyledLink = styled(Link)`
+  text-decoration: none !important;
+`
+
+const App = (): JSX.Element => {
+  const isAuthenticated = useSelector((state: any): boolean => {
+    return state.authSlice.isAuthenticated
+  })
+
+  useEffect(() => {
+    //mutate isAuthenticated globaly
+    auth.checkAuthentication()
+  }, [])
+
+
   return (
     <Box
       px="10vw"
     >
       <Router>
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/send">
-            <StyledLink to="/"><IconButton><ArrowBackIcon/></IconButton></StyledLink>
-            <Send/>
-          </Route>
-          <Route exact path="/receive">
-            <StyledLink to="/"><IconButton><ArrowBackIcon/></IconButton></StyledLink>
-            <Receive/>
-          </Route>
-          <Route exact path="/history">
-            <StyledLink to="/"><IconButton><ArrowBackIcon/></IconButton></StyledLink>
-            <HistoryC/>
-          </Route>
+          {[
+            isAuthenticated  && loggedInRoutes,
+            !isAuthenticated && notLogedInRoutes,
+          ]}
         </Switch>
       </Router>
     </Box>
   )
 }
 
-const StyledLink = styled(Link)`
-  text-decoration: none !important;
-`
+const notLogedInRoutes = (
+  <Box key="unloggedIn">
+    <Route exact path="/">
+      <FirstPage />
+    </Route>
+    <Route exact path="/generate-wallet">
+      <StyledLink to="/"><IconButton><ArrowBackIcon/></IconButton></StyledLink>
+      <GenerateWallet />
+    </Route>
+    <Redirect to="/"/>
+  </Box>
+)
+
+const handleLockWallet = () => {
+  auth.deAuthenticate()
+}
+const loggedInRoutes = (
+  <Box key="loggedIn">
+    <Route exact path="/">
+      <Box textAlign="end">
+        <IconButton onClick={handleLockWallet}>
+          <LockIcon/>
+        </IconButton>
+      </Box>
+      <Home />
+    </Route>
+    <Route exact path="/send">
+      <StyledLink to="/"><IconButton><ArrowBackIcon/></IconButton></StyledLink>
+      <Send/>
+    </Route>
+    <Route exact path="/receive">
+      <StyledLink to="/"><IconButton><ArrowBackIcon/></IconButton></StyledLink>
+      <Receive/>
+    </Route>
+    <Route exact path="/history">
+      <StyledLink to="/"><IconButton><ArrowBackIcon/></IconButton></StyledLink>
+      <HistoryC/>
+    </Route>
+    <Redirect to="/"/>
+  </Box>
+)
 
 export default App;
