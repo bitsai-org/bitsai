b2a2f1c31d687e06b7c7c1195a4dd20e46d06682
Adoliin <ben.meft0@gmail.com>
2021-10-23 15:49:11 +0100

    add GenerateWallet's MnemonicInfo

---
+++ src/components/GenerateWallet/MnemonicInfo.tsx
@@ -0,0 +1,65 @@
+import Box from '@material-ui/core/Box'
+import EcoIcon from '@material-ui/icons/Eco';
+
+interface Props {
+  mnemonicSeed: string,
+}
+
+const MnemonicInfo = (props: Props): JSX.Element => {
+  return (
+    <Box mt="2rem">
+      <Box
+        mb="0.2rem"
+        display="flex"
+        alignItems="center"
+      >
+        <EcoIcon/>
+        <i>Recovery Seed phrase</i>
+      </Box>
+      <Box>
+        <Box
+          border={2}
+          borderRadius={8}
+          //borderColor="#ebc19c"
+          p="1rem"
+        >
+          <h2>
+            {props.mnemonicSeed}
+          </h2>
+        </Box>
+      </Box>
+      <Box
+        fontSize="1.3rem"
+        mt="2rem"
+      >
+        <p>
+          Please save these 12 words on paper (order is important).
+          This seed will allow you to recover your wallet in case of
+          computer failure.
+        </p>
+        <Box
+          component="h4"
+          mt="1rem"
+        >
+          WARNING:
+        </Box>
+        <Box
+          component="ul"
+          mt="0.3rem"
+          pl="2.5rem"
+        >
+          <li>Never disclose your seed.</li>
+          <li>Never type it on a website.</li>
+          <li>Do not store it electronically.</li>
+          <li>
+            In case you decide to store it electronically anyway: try
+            to store it in an encrypted way so that only you can decrypt
+            it and access this seed phrase.
+          </li>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default MnemonicInfo;
