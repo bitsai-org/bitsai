5438ea9e1406e11eae68a3ee51d2d5608eaa381d
Adoliin <ben.meft0@gmail.com>
2021-10-23 16:18:32 +0100

    add Redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Provider} from 'react-redux'
+
 import App from './App';
+import store from './store/store'
+import './index.css';
 import reportWebVitals from './reportWebVitals';
-//import styled from 'styled-components'
-
-{/*const AppStyled = styled(App)`
-  body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-
-    background-color: #a35c20;
-    color: #ebc19c;
-  }
-
-  * {
-    margin: 0;
-    padding:0;
-    box-sizing: border-box;
-  }
-`*/}
-
 ReactDOM.render(
   //<React.StrictMode>
-    <App />,
-    //<AppStyled/>
+    <Provider store={store}>
+      <App />
+    </Provider>,
   //</React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
